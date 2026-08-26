6a3bdcfc6cfc2a833b1e3aa427592f7ec1781b9c
CtrlLuzzio <luis.montilla.ag@outlook.com>
2026-08-25 20:36:27 -0400

    hypr/feat: Added hyprland-session.target to autostart and mouse theme to env
    Also fixed wl-clipboard autostart

--- hypr/modules/env.lua
+++ hypr/modules/env.lua
@@ -1,6 +1,8 @@
 -- https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 hl.env("XCURSOR_SIZE", "22")
 hl.env("HYPRCURSOR_SIZE", "22")
+hl.env("XCURSOR_THEME", "Bibata-Modern-Ice")
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Ice")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
